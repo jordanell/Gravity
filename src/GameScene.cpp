502c88497ee9589e3aed854f27469b831eace1dd
Jordan Ell <jordan.ell7@gmail.com>
2011-11-10 12:59:11 -0800

    Got rid of while loop from testing.

--- src/GameScene.cpp
+++ src/GameScene.cpp
@@ -17,7 +17,7 @@ namespace ManhattanProject
 
 		this->Initialize();
 	}
-	
+
 	GameScene::~GameScene()
 	{
 		delete Map;
@@ -26,16 +26,13 @@ namespace ManhattanProject
 	void GameScene::Initialize()
 	{
 		// Create Tile Map
-		while(1)
-		{
 		Map = new TileMap(game, Camera(Vector2(0, 0), Vector2(400, 600)), GameFramework::Rectangle(-1000,-1000,2000,2000));
 
 		Map->AddTileLayer();
 
 		Map->PrintLayers();
-		
+
 		delete Map;
-		}
 
 		Scene::Initialize();
 	}
