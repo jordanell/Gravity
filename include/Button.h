8fcfc20cbf29587f4622aabcad934240f111dc43
Braden Simpson <braden@uvic.ca>
2011-12-17 01:38:58 -0800

    more work on GuiObject.  consider adding a special Layer that is a GUI Layer which is always drawn.

--- include/Button.h
+++ include/Button.h
@@ -4,31 +4,30 @@
 //
 //  Created by Braden Simpson on 11-12-16.
 //  Copyright 2011 __MyCompanyName__. All rights reserved.
-//
 
-#ifndef Game_braden_Button_h
-#define Game_braden_Button_h
+#ifndef BUTTON_H
+#define BUTTON_H
 
 #include "GameFramework.h"
-#include "Scene.h"
+#include "GuiObject.h"
 
 using namespace GameFramework;
 
 namespace ManhattanProject
 {
-	class Button : public DrawableGameComponent
+	class Button : public GuiObject
 	{
-    public:
-        Button (Texture2D* background, Game* game);
-        //Button (Texture2D* background, Texture2D* hover, Texture2D* active);
-        void Initialize();
-        void Update();
-        void Draw();
+        public:
+            Button(Game* game);
+            Button(Game* game, Texture2D* background, float s, float rot, Vector2 pos, Color col);
+            void Initialize();
+            void Update();
+            void Draw();
         
-    protected:
-        Texture2D* background;
-        Texture2D* hover;
-        Texture2D* active;
+        protected:
+            Texture2D* background;
+            Texture2D* hover;
+            Texture2D* active;
 	};
 }
 
--- include/GuiObject.h
+++ include/GuiObject.h
@@ -6,9 +6,35 @@
 //  Copyright 2011 __MyCompanyName__. All rights reserved.
 //
 
-#ifndef Game_braden_GuiObject_h
-#define Game_braden_GuiObject_h
+#ifndef GUIOBJECT_H
+#define GUIOBJECT_H
 
+#include "GameFramework.h"
+#include "Camera.h"
+#include "DrawableGameComponent.h"
 
+using namespace GameFramework;
 
+namespace ManhattanProject
+{
+    class GuiObject: public DrawableGameComponent
+    {
+        public:
+            GuiObject(Game* game);
+            GuiObject(Game* game, Texture2D* background, float s, float rot, Vector2 pos, Color col);
+            void Initialize();
+            void Update();
+            void Draw(Camera camera);
+            float scale;
+            float rotation;
+            Vector2 position;
+            Color color;
+
+        protected:
+            Texture2D* background;
+            Texture2D* hover;
+            Texture2D* active;
+            Game* game;
+    };
+}
 #endif
